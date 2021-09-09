ec7659996aa3da3fb20c4fcd184e3ac335b8a072
chimdie <nzechimdi@yahoo.com>
2021-09-09 23:54:19 +0100

    feat/server and details

---
+++ server/server.js
@@ -0,0 +1,12 @@
+import path from "path";
+import express from "express";
+import initialRenderRoutes from "./routes/index";
+
+const app = express();
+app.use("^/$", initialRenderRoutes);
+
+app.use(express.static(path.resolve(__dirname, "../build")));
+
+const port = 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
